327e328e01ebed6ece4c0927ed7e477ee3850224
Miracle <18365050+miracle-kang@users.noreply.github.com>
2020-06-20 01:03:57 +0800

    Fix: 单词拼写错误

--- src/leetcode/decode_string_test.go
+++ src/leetcode/decode_string_test.go
@@ -9,24 +9,24 @@ func TestDecodeString(t *testing.T) {
 	condition = "3[a]2[bc]"
 	str = decodeString(condition)
 	if str != "aaabcbc" {
-		t.Error("Condition ", condition, " unexcepted result: ", str)
+		t.Error("Condition ", condition, " unexpected result: ", str)
 	}
 
 	condition = "3[a2[c]]"
 	str = decodeString(condition)
 	if str != "accaccacc" {
-		t.Error("Condition ", condition, " unexcepted result: ", str)
+		t.Error("Condition ", condition, " unexpected result: ", str)
 	}
 
 	condition = "2[abc]3[cd]ef"
 	str = decodeString(condition)
 	if str != "abcabccdcdcdef" {
-		t.Error("Condition ", condition, " unexcepted result: ", str)
+		t.Error("Condition ", condition, " unexpected result: ", str)
 	}
 
 	condition = "abc3[cd]xyz"
 	str = decodeString(condition)
 	if str != "abccdcdcdxyz" {
-		t.Error("Condition ", condition, " unexcepted result: ", str)
+		t.Error("Condition ", condition, " unexpected result: ", str)
 	}
 }
