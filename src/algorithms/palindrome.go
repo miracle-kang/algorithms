20895125ea464cb04782304cdb5229c651040595
Miracle <18365050+miracle-kang@users.noreply.github.com>
2020-06-19 12:04:18 +0800

    验证回文串

---
+++ src/algorithms/palindrome.go
@@ -0,0 +1,35 @@
+package algorithms
+
+import (
+	"strings"
+	"unicode"
+)
+
+func isPalindrome(s string) bool {
+	str := strings.ToLower(s)
+	i := 0
+	j := len(str) - 1
+	for i < j {
+		for i < len(str) && !isChar(str[i]) {
+			i++
+		}
+		for j > 0 && !isChar(str[j]) {
+			j--
+		}
+
+		// 如果是空字符串
+		if i >= len(str) || j < 0 {
+			return true
+		}
+		if str[i] != str[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func isChar(c byte) bool {
+	return unicode.IsLetter(rune(c)) || unicode.IsDigit(rune(c))
+}
---
+++ src/algorithms/palindrome_test.go
@@ -0,0 +1,24 @@
+package algorithms
+
+import "testing"
+
+/*
+ * TestPalindrome
+ *
+ *  验证回文串
+ */
+func TestPalindrome(t *testing.T) {
+	var str = "A man, a plan, a canal: Panama"
+	var result = isPalindrome(str)
+
+	t.Log(isChar(':'))
+	if result != true {
+		t.Error("执行结果不符合预期")
+	}
+
+	str = "race a car"
+	result = isPalindrome(str)
+	if result != false {
+		t.Error("执行结果不符合预期")
+	}
+}
