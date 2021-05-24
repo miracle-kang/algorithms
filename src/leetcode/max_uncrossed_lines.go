9e8ce17d19a9079179a81f928e21338a12ae1a73
Miracle <18365050+miracle-kang@users.noreply.github.com>
2021-05-24 15:18:41 +0800

    Leetcode #1035 优化

--- src/leetcode/max_uncrossed_lines.go
+++ src/leetcode/max_uncrossed_lines.go
@@ -30,3 +30,31 @@ func maxUncrossedLines(nums1 []int, nums2 []int) int {
 	}
 	return dp[n][m]
 }
+
+// 压缩数组优化
+func maxUncrossedLines1(nums1 []int, nums2 []int) int {
+	if len(nums1) < len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	m := len(nums2)
+	dp := make([]int, m+1)
+	next := make([]int, m+1)
+	for i := range nums1 {
+		for j := range nums2 {
+			if nums1[i] == nums2[j] {
+				next[j+1] = dp[j] + 1
+			} else {
+				next[j+1] = max(dp[j+1], next[j])
+			}
+		}
+		dp, next = next, dp
+	}
+	return dp[m]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
