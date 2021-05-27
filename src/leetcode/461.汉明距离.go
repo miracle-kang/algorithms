f4d422d4f6a2784818defaae05969b3c3e0d8930
Miracle <18365050+miracle-kang@users.noreply.github.com>
2021-05-27 10:28:52 +0800

    refactor: Leetcode #461 优化计算二进制1位数

--- src/leetcode/461.汉明距离.go
+++ src/leetcode/461.汉明距离.go
@@ -8,13 +8,19 @@
 func hammingDistance(x int, y int) int {
 	z := x ^ y
 	cnt := 0
-	for z > 0 {
-		if z & 1 == 1 {
-			cnt++
-		}
-		z = z >> 1
+	for ; z > 0; z&=z-1 {
+		cnt++
 	}
 	return cnt
 }
+
 // @lc code=end
 
+func hammingDistance1(x int, y int) int {
+	z := x ^ y
+	cnt := 0
+	for ; z > 0; z>>=1 {
+		cnt += z&1
+	}
+	return cnt
+}
