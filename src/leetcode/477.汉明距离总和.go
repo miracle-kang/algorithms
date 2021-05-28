c2413bb0ac636f7089b915619011261e26327b9b
Miracle <18365050+miracle-kang@users.noreply.github.com>
2021-05-28 10:28:27 +0800

    refactor: 优化Leetcode #477

--- src/leetcode/477.汉明距离总和.go
+++ src/leetcode/477.汉明距离总和.go
@@ -8,9 +8,12 @@
 func totalHammingDistance(nums []int) int {
 	n := len(nums)
 	res := 0
-	for i := 0; i < 32; i++ {
+	for i := 0; i < 30; i++ {
 		c := 0
 		for _, num := range nums {
+			if num <= 0 {
+				continue
+			}
 			c += (num >> i) & 1
 		}
 		res += c * (n - c) // 每一位中 1的个数 * 0的个数
